33507a8e9b2c5530de8feefa1db0033caa2d7715
Johan Grobler <johan@Quench.mobi>
2024-10-30 09:50:04 +0200

    Spel

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 func main() {
 	app := fiber.New()
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World!")
+		return c.SendString("Hello, World!!")
 	})
 	app.Get("/up", func(c *fiber.Ctx) error {
 		return c.JSON(fiber.Map{"status": "up"})
