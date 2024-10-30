ee3dbc4c0d68f40a4cd3f1a9f6d2e310706001da
Johan Grobler <johan@Quench.mobi>
2024-10-30 10:49:06 +0200

    Hello Host name

--- main.go
+++ main.go
@@ -1,17 +1,22 @@
 package main
 
 import (
+	"os"
+
 	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
-	//create app
+
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World!")
+		hostname, err := os.Hostname()
+		if err != nil {
+
+		}
+		return c.SendString("Hello from go running on: " + hostname)
 	})
 
-	// run app
 	app.Listen(":8080")
 }
